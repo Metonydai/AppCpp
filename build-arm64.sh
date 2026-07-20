6caaf0aea47877fd16c30c56c8ce2fcc79d2f468
sherryani <kusuje@gmail.com>
2026-07-20 10:38:44 +0800

    Cross compliation completed

---
+++ build-arm64.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DEFAULT_BUILD_TYPE="Debug"
+
+BUILD_TYPE="${1:-$DEFAULT_BUILD_TYPE}"
+
+echo "[INFO] Build Type is set to: ${BUILD_TYPE}"
+
+# 建立 build 資料夾（-p 確保資料夾存在時不報錯，並自動建立父資料夾）
+mkdir -p build-arm64
+
+# 進入 build 資料夾並執行 CMake
+echo "[INFO] Running CMake..."
+# 執行 CMake 設定階段 (使用你的 default preset)
+# cmake --preset=default
+cmake -B build-arm64 -DCMAKE_TOOLCHAIN_FILE=toolchain-arm64.cmake
+
+# 執行編譯階段
+cmake --build build-arm64 --config "${BUILD_TYPE}"
+
+echo "[INFO] Done."
